9912b2fd204a5fb6e4838d1277a02415419a3e33
baget <oren@weil.org.il>
2015-12-15 21:28:47 +0200

    Update License

--- BMP085Lib/BMP085.cs
+++ BMP085Lib/BMP085.cs
@@ -7,7 +7,6 @@
 **	Author: Oren Weil 
 **	
 **	The MIT License (MIT)
-**	Copyright (c) 2015 Oren Weil
 **	Permission is hereby granted, free of charge, to any person obtaining a copy
 **	of this software and associated documentation files (the "Software"), to deal
 **	in the Software without restriction, including without limitation the rights
